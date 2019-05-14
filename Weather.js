861204e334920d7de17dd0814e4cd85461b566a3
lgh7418 <lgh7418@gmial.com>
2019-05-14 21:04:15 +0900

    11. Building the Weather View + Icons

---
+++ Weather.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { LinearGradient } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+
+export default class Weather extends Component {
+  render() {
+    return (
+      <LinearGradient colors={["#00c6fb", "#005bea"]} style={styles.container}>
+        <StatusBar hidden={true} />
+        <View style={styles.upper}>
+          <Ionicons color="white" size={144} name="ios-rainy" />
+          <Text style={styles.temp}>35℃</Text>
+        </View>
+        <View style={styles.lower}>
+          <Text style={styles.title}>Raining like a MF</Text>
+          <Text style={styles.subtitle}>For more info look outside</Text>
+        </View>
+      </LinearGradient>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  temp: {
+    fontSize: 48,
+    color: "white",
+    marginTop: 10
+  },
+  lower: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  title: {
+    fontSize: 38,
+    color: "white",
+    marginBottom: 10,
+    fontWeight: "300"
+  },
+  subtitle: {
+    fontSize: 24,
+    color: "white",
+    marginBottom: 24
+  }
+});
