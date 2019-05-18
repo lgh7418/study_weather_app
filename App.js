8664e86db95441c8bbed56cee814fd04b97c204a
lgh7418 <lgh7418@gmial.com>
2019-05-18 22:37:49 +0900

    14. Getting the Weather

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Weather from "./Weather";
 
+const API_KEY = "3b080cd731e12485a9fca9bb41f8e992";
+
 export default class App extends Component {
   state = {
     isLoaded: false,
-    error: null
+    error: null,
+    temperature: null,
+    name: null
   };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
         this.setState({
@@ -22,6 +24,18 @@ export default class App extends Component {
       }
     );
   }
+
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
 
   render() {
     const { isLoaded, error } = this.state;
